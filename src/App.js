19d4912580463595723cddb67c212832bd3158f7
Pranjal Dwivedi <pradwd6392@gmail.com>
2021-06-21 12:53:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React,{useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
-
+import Paisa from './paisa';
 function App() {
+  const[coins,setCoins]=useState([]);
+  const[search,setSearch]=useState('');
+
+  useEffect(() =>{
+    axios.get(
+          'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+      ).then(res => {
+      setCoins(res.data);
+    }).catch(error => console.log(error));
+  },[]);
+
+  const handleChange= e => {
+    setSearch(e.target.value)
+  };
+  
+  const filteredCoins = coins.filter(
+    coin => 
+    coin.name.toLowerCase().includes(search.toLowerCase())
+   
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coint-text">Search For Coin</h1>
+        <form >
+          <input type ='text' placeholder='Search' className='coin-input'
+          onChange={handleChange}/>
+        </form>
+      </div>
+      {
+        filteredCoins.map( coin => {
+          return( 
+          <Paisa 
+            key={coin.id} 
+            name={coin.name}
+            image ={coin.image}
+            symbol ={coin.symbol}
+            price = {coin.market_cap}
+            volume ={coin.current_price}
+            marketcap={coin.total_volume}
+            pricechange={coin.price_change_percentage_24h}
+            />
+          );
+        })
+      }
+  </div>
+
   );
 }
-
 export default App;
---
+++ src/paisa.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './paisa.css';
+
+const paisa = ({ name,image, symbol, price, volume,marketcap,pricechange}) => {
+    return (
+        <div className='coin-container'>
+            <div className="coin-row">
+                <div className="coins">
+                    <img src={image} alt="crypto"/>
+                    <h1>{name}</h1>
+                    <p className="symbol">{symbol}</p>
+                </div>
+                <div className="coin-data">
+                    <p className="coin-price">${price}</p>
+                    <p className="coin-volume">${volume.toLocaleString()}</p>
+                    {
+                        pricechange < 0 ? 
+                            (<p className="code-percent red" >{pricechange.toFixed(2)}%</p>) 
+                            : (<p className="code=percent green">{pricechange.toFixed(2)}%</p> )
+                    }
+                    <p className='coin-marketcap'>
+            Mkt Cap: ${marketcap.toLocaleString()}
+          </p>
+                </div>
+            </div>
+        </div>
+    )
+} 
+
+export default paisa;
